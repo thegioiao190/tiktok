7345e495b6868a6d1dabc79a39d2a81e5423ccbf
MinPink <thegioiao@gmail.com>
2022-10-07 14:10:51 +0700

    117. Xây dựng UI phần sidebar #3

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -5,26 +5,35 @@ import PropTypes from 'prop-types';
 import Image from '../Image';
 
 import style from './SuggestedAccounts.module.scss';
+import SuggestedPopper from './SuggestedPopper';
 
 const cx = classNames.bind(style);
 
-function AccountItem({ title }) {
+const data = {
+    avatar: 'https://via.placeholder.com/150',
+    username: 'nguyenvana',
+    name: 'Nguyen Van A',
+    icon: <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />,
+    followerValue: '7.4M',
+    likeValue: '411.2M',
+};
+function AccountItem({}) {
     return (
-        <div className={cx('account-item')}>
-            <Image className={cx('avatar')} src="https://via.placeholder.com/150" alt="" />
-            <div className={cx('info')}>
-                <p className={cx('username')}>
-                    nguynvana
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </p>
-                <p className={cx('name')}>nguyen van a</p>
+        <SuggestedPopper {...data}>
+            <div className={cx('account-item')}>
+                <Image className={cx('avatar')} src={data.avatar} alt="" />
+                <div className={cx('info')}>
+                    <p className={cx('username')}>
+                        {data.username}
+                        {data.icon}
+                    </p>
+                    <p className={cx('name')}>{data.name}</p>
+                </div>
             </div>
-        </div>
+        </SuggestedPopper>
     );
 }
 
-AccountItem.propsTypes = {
-    title: PropTypes.string.isRequired,
-};
+AccountItem.propsTypes = {};
 
 export default AccountItem;
---
+++ src/components/SuggestedAccounts/SuggestedPopper.js
@@ -0,0 +1,58 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+import Image from '../Image';
+
+import style from './SuggestedAccounts.module.scss';
+
+const cx = classNames.bind(style);
+
+function SuggestedPopper({ avatar, username, name, icon, followerValue, likeValue, children }) {
+    const renderPopper = () => {
+        return (
+            <div className={cx('popper-wrapper')}>
+                <div className={cx('popper-header')}>
+                    <Image className={cx('avatar')} src={avatar} alt="" />
+                    <Button className={cx('follow-btn')} primary>
+                        Follow
+                    </Button>
+                </div>
+                <div className={cx('popper-info')}>
+                    <p className={cx('username')}>
+                        {username}
+                        {icon}
+                    </p>
+                    <p className={cx('name')}>{name}</p>
+                </div>
+                <div className={cx('popper-analytics')}>
+                    <p className={cx('followers-value')}>{followerValue}</p>
+                    <p className={cx('label')}>Followers</p>
+                    <p className={cx('like-value')}>{likeValue}</p>
+                    <p className={cx('label')}>Likes</p>
+                </div>
+            </div>
+        );
+    };
+    return (
+        <div>
+            <Tippy interactive offset={[-8, 0]} delay={[800, 50]} placement="bottom-start" render={renderPopper}>
+                {children}
+            </Tippy>
+        </div>
+    );
+}
+
+SuggestedPopper.propTypes = {
+    children: PropTypes.object.isRequired,
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    icon: PropTypes.node.isRequired,
+    followerValue: PropTypes.string.isRequired,
+    likeValue: PropTypes.string.isRequired,
+};
+
+export default SuggestedPopper;
